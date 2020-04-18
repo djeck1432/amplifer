76e9c7676dab92eae89fd74bf050516995a1ba45
djeck1432 <oleh.kostashchuk@gmail.com>
2020-04-18 13:50:31 +0200

    made new exception caught

--- vkontakte.py
+++ vkontakte.py
@@ -4,7 +4,15 @@ from dotenv import load_dotenv
 import datetime
 
 
-def get_payload(vk_token,offset):
+def check_response(response):
+    if 'response' in response.json():
+        response_json = response.json()['response']
+        return response_json
+    else:
+        raise requests.HTTPError(response.json()['error']['error_msg'])
+
+
+def get_payload(vk_token, offset):
     payload = {
         'access_token': vk_token,
         'v': '5.103',
@@ -14,25 +22,22 @@ def get_payload(vk_token,offset):
     return payload
 
 
-def fetch_posts(vk_token,vk_group_name):
+def fetch_posts(vk_token, vk_group_name):
     url = 'https://api.vk.com/method/wall.get'
     offset = 0
     count_posts = 100
     posts_ids = []
     while offset < count_posts:
-        extra_payload = {'domain': vk_group_name,'filter': 'owner'}
-        payload = get_payload(vk_token,offset)
+        extra_payload = {'domain': vk_group_name, 'filter': 'owner'}
+        payload = get_payload(vk_token, offset)
         payload.update(extra_payload)
         response = requests.post(url=url, data=payload)
-        response_json = response.json()['response']
-        if 'error' in response_json:
-            return response_json['error']
-        else:
-            offset += 100
-            count_posts = response_json['count']
-            post_items = response_json['items']
-            post_ids = [post_items[post_number]['id'] for post_number, item in enumerate(post_items)]
-            posts_ids.extend(post_ids)
+        response_json = check_response(response)
+        offset += 100
+        count_posts = response_json['count']
+        post_items = response_json['items']
+        post_ids = [post_items[post_number]['id'] for post_number, item in enumerate(post_items)]
+        posts_ids.extend(post_ids)
     return posts_ids
 
 
@@ -44,11 +49,8 @@ def get_group_id(vk_token, vk_group_name):
         'v': '5.103'
     }
     response = requests.get(url=url, params=params)
-    response_json = response.json()['response']
-    if 'error' in response_json:
-        return response_json['error']
-    else:
-        return f"-{response_json[0]['id']}"
+    response_json = check_response(response)
+    return f"-{response_json[0]['id']}"
 
 
 def fetch_commetns(vk_token, post_id, group_id):
@@ -61,17 +63,13 @@ def fetch_commetns(vk_token, post_id, group_id):
         payload = get_payload(vk_token, offset)
         payload.update(extra_payload)
         response = requests.post(url=url, data=payload)
-        response_json = response.json()['response']
-        if 'error' in response_json:
-            return response_json['error']
-        else:
-
-            offset += 100
-            count_comments = response_json['count']
-            comments_count = len(response_json['items'])
-            if comments_count > 0:
-                for comment_number in range(comments_count):
-                    comments.append(response_json['items'][comment_number])
+        response_json = check_response(response)
+        offset += 100
+        count_comments = response_json['count']
+        comments_count = len(response_json['items'])
+        if comments_count > 0:
+            for comment_number in range(comments_count):
+                comments.append(response_json['items'][comment_number])
     return comments
 
 
@@ -105,14 +103,10 @@ def fetch_all_likes(vk_token, group_id, post_id):
         payload = get_payload(vk_token, offset)
         payload.update(extra_payload)
         response = requests.post(url=url, data=payload)
-        response_json = response.json()['response']
-        if 'error' in response_json:
-            return response_json['error']
-        else:
-            offset += 100
-            count_likes = response_json['count']
-            likes.extend(response_json['items'])
-
+        response_json = check_response(response)
+        offset += 100
+        count_likes = response_json['count']
+        likes.extend(response_json['items'])
     return set(likes)
 
 
